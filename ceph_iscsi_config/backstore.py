52713194a672601093eb7602a878fabe6ebbb06b
Ricardo Marques <rimarques@suse.com>
2019-02-07 18:59:51 +0000

    Adds support for kernel lio backstore
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- ceph_iscsi_config/backstore.py
+++ ceph_iscsi_config/backstore.py
@@ -1,13 +1,16 @@
-from rtslib_fb import UserBackedStorageObject
+from rtslib_fb import UserBackedStorageObject, RBDStorageObject
 
 from ceph_iscsi_config.utils import CephiSCSIError
 
 USER_RBD = 'user:rbd'
+RBD = 'rbd'
 
 
 def lookup_storage_object(name, backstore):
     if backstore == USER_RBD:
         return UserBackedStorageObject(name=name)
+    elif backstore == RBD:
+        return RBDStorageObject(name=name)
     else:
         raise CephiSCSIError("Could not lookup storage object - "
                              "Unsupported backstore {}".format(backstore))
